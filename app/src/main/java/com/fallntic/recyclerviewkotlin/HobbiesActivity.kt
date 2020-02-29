4e1e471e6b40ac44698d52380f26f512bbe077ba
fallaw90 <37890292+fallaw90@users.noreply.github.com>
2020-02-29 18:40:23 -0500

    Initial commit

---
+++ app/src/main/java/com/fallntic/recyclerviewkotlin/HobbiesActivity.kt
@@ -0,0 +1,32 @@
+package com.fallntic.recyclerviewkotlin
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.activity_hobbies.*
+
+
+class HobbiesActivity : AppCompatActivity() {
+
+    companion object {
+        val TAG: String = HobbiesActivity::class.java.simpleName
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_hobbies)
+
+        setupRecyclerView()
+    }
+
+    private fun setupRecyclerView() {
+
+        val layoutManager = LinearLayoutManager(this)
+        layoutManager.orientation = LinearLayoutManager.VERTICAL
+        recyclerView.layoutManager = layoutManager
+
+        val adapter= HobbiesAdapter(this, Supplier.hobbies)
+        recyclerView.adapter = adapter
+    }
+}
